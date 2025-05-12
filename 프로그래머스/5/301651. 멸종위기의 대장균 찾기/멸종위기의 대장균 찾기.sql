9cfc7908c4c2260ba65b8fedc5f59cbc7edad6d3
eunseoKim <eunseokim43@gmail.com>
2025-05-12 15:23:11 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -0,0 +1,23 @@
+WITH RECURSIVE 
+tmp AS (
+    -- 1단계 : 부모가 없는 루트 노드
+    SELECT ID, PARENT_ID, 1 AS GENERATION
+      FROM ecoli_data
+     WHERE PARENT_ID IS NULL
+    
+    UNION ALL
+
+    -- 2단계 : 자식을 따라가며 세대 증가
+    SELECT c.ID, c.PARENT_ID, p.generation + 1
+      FROM ecoli_data c
+     inner JOIN tmp p ON c.PARENT_ID = p.ID
+)
+
+-- 3단계 : 부모ID에 해당되지 않는 ID 조회
+SELECT COUNT(*) AS COUNT, GENERATION
+  FROM tmp
+ WHERE ID NOT IN (SELECT PARENT_ID
+                    FROM tmp 
+                   WHERE PARENT_ID IS NOT NULL)
+ GROUP BY GENERATION
+ ORDER BY GENERATION;
