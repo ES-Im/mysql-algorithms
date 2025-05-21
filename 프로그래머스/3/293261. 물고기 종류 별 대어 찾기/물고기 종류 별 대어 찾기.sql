c1d585e571e18fe42abf879dcfe239bd32f71938
eunseoKim <eunseokim43@gmail.com>
2025-05-21 19:17:12 +0900

    Update 물고기 종류 별 대어 찾기.sql

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,8 +1,3 @@
-# select any_value(ID), FISH_NAME, MAX(LENGTH)
-#   from fish_info f
-#   join fish_name_info fn on f.fish_type = fn.fish_type
-#   group by fn.FISH_NAME
-
 select f.id id, fi.fish_name fish_name, f.length length
 from fish_info f
 join  (select fish_type, MAX(LENGTH) as length
@@ -10,3 +5,12 @@ join  (select fish_type, MAX(LENGTH) as length
                           group by fish_type) tmp
 on f.fish_type = tmp.fish_type and f.length = tmp.length
 join fish_name_info fi on f.fish_type = fi.fish_type
+
+SELECT f.id, fi.fish_name, f.length
+FROM (
+    SELECT *, ROW_NUMBER() OVER (PARTITION BY fish_type ORDER BY length DESC) as rn
+    FROM fish_info
+) f
+JOIN fish_name_info fi ON f.fish_type = fi.fish_type
+WHERE f.rn = 1
+order by ID;
