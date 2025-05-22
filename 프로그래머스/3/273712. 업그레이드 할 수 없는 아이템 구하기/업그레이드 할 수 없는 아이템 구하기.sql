e1efb4153fd9ca642d9c16a98a7aeea96977a8ce
eunseoKim <eunseokim43@gmail.com>
2025-05-22 19:07:09 +0900

    [level 3] Title: 업그레이드 할 수 없는 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
@@ -0,0 +1,12 @@
+with getId as (
+    select item_id from item_tree t1
+    where not exists (
+        select 1 from item_tree t2
+        where t1.item_id = t2.parent_item_id
+    )
+)
+
+select i.item_id, ITEM_NAME, RARITY
+  from item_info i
+  join getId g on i.item_id = g.item_id
+ order by 1 desc;
