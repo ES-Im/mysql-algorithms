5165490599bd9c4fc7e204372f5a99049848d303
eunseoKim <eunseokim43@gmail.com>
2025-06-30 16:25:41 +0900

    Update 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -10,14 +10,15 @@
 #      limit 1
 # )
 
-
+#→ 아래는 이건 group by 없이 집계 함수와 일반 컬럼을 같이 쓰는 형태로,
+#→ BOARD_ID가 MAX(views)의 게시글이 아닐 수도 있음!
 # select concat('/home/grep/src/', BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT) FILE_PATH
 #   from USED_GOODS_FILE
 #   where board_id = (select BOARD_ID from mostView)
 #     and FILE_ID like 'IMG%'
 #   order by FILE_ID desc;
 
-
+# 아래와 같이 이런 문제에서는 집계함수보다 정렬 + LIMIT이 안전
 SELECT CONCAT('/home/grep/src/', BOARD_ID, '/',FILE_ID, FILE_NAME, FILE_EXT)
 FROM USED_GOODS_FILE A 
 WHERE BOARD_ID = (SELECT BOARD_ID
